987358d429c5366bb84d3f0cb992cba28f6e282f
xisquito03 <xisco034@gmail.com>
2022-10-10 10:41:30 +0200

    sssss

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public int playerAge = 33;
+    private float playerSpeed = 3.125f;
+    public string playerName = "Xisco Medrano";
+    private bool isGameOver = false;
+
+}
